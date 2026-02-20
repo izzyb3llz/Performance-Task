f9faf502174525dbe10a42943093b39d3f1474b9
izzyb3llz <isacontreras519@gmail.com>
2026-02-20 20:20:23 +0000

    performance task

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ if choice == 'no'
     active = False
     break
 
-elif choice in coffee_menu:
+elif choice in coffee_menu: 
     # checls if users choice is availibe in the menu
     if choice == "Iced latte"
     tell_customer("Ok, we can do that!")
@@ -29,5 +29,33 @@ elif choice == "Americano"
 elif choice == "Cappuccino"
     tell_customer("Ok, we can do that")
 
-    def tell_customer(setup, punchline)
+remove_choice = input("Which coffee would you like?: ").lower()
+# askes user if theres a menu option they would not like.
+if remove_choice in coffee_menu: 
+#removes existing menu option
+    coffee_menu.remove(remove_choice)
+    print(f"Ok, choose another item that's not {remove_choice}.")
+else:
+    print(f"OK, is that what you would like?")
+get_rating() 
+# calls the function for user feedback
+another = print(f"Would you like something else?: ").lower()
+if another != "yes":
+    print(f"OK, bye bye!")
+    break
+
+else:
+    print(f"Sorry, we don't carry that.")
+# Tells customer what they want isn't a menu option.
+
+def tell_customer(setup, punchline):
+    input("Hello, welcome to coffe shop! What can I get you?")
+    input(f"{setup}...")
+    # Pauses for customer response
+    print(f"{punchline}\n")
+    # Delivers punchline
+
+
+
+
 
