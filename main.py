55cb775c0fff316671ac2519f9044826e915801a
izzyb3llz <isacontreras519@gmail.com>
2026-02-26 21:12:54 +0000

    edits

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Perfomrance Task
 
 # manages the menu telling loop and user interation
-def run_coffee_menu(coffee_menu):
+def coffee_menu(coffee_menu):
     active = True
     # determines whether loops stays open and running or not
     while active: 
@@ -10,51 +10,51 @@ def run_coffee_menu(coffee_menu):
             print("Sorry, we don't carry that.")
             break
 
-print(f"We do carry these drinks though: {coffee_menu}")
-# displays what drink coffee shop has to offer
-choice = input("Would you like to order anything? If not, type 'no' :").strip().lower()
-# gives user the option of ordering something off the menu
+    print(f"We do carry these drinks though: {coffee_menu}")
+    # displays what drink coffee shop has to offer
+    choice = input("Would you like to order anything? If not, type 'no' :").strip().lower()
+    # gives user the option of ordering something off the menu
+    import sys
+    if choice == 'no':
+    # loop is exited
+        active = False
+        sys.exit()
 
-if choice == 'no'
-# loop is exited
-    active = False
-    break
-
-elif choice in coffee_menu: 
+def tell_customer(tell_customer, choice, get_rating):
+    if choice == "Iced latte":
     # checls if users choice is availibe in the menu
-    if choice == "Iced latte"
-    tell_customer("Ok, we can do that!")
-elif choice == "Americano"
-    tell_customer("Ok, we can do that!")
-elif choice == "Cappuccino"
-    tell_customer("Ok, we can do that")
+        tell_customer("Ok, we can do that!")
+    elif choice == "Americano":
+        tell_customer("Ok, we can do that!")
+    elif choice == "Cappucino":
+        tell_customer("Ok, we can do that")
 
-remove_choice = input("Which coffee would you like?: ").lower()
-# askes user if theres a menu option they would not like.
-if remove_choice in coffee_menu: 
-#removes existing menu option
-    coffee_menu.remove(remove_choice)
-    print(f"Ok, choose another item that's not {remove_choice}.")
-else:
-    print(f"OK, is that what you would like?")
-get_rating() 
-# calls the function for user feedback
-another = print(f"Would you like something else?: ").lower()
-if another != "yes":
-    print(f"OK, bye bye!")
-    break
+    remove_choice = input("Which coffee would you like?: ").lower()
+    # askes user if theres a menu option they would not like.
+    if remove_choice in coffee_menu: 
+        #removes existing menu option
+        coffee_menu.remove(remove_choice)
+        print(f"Ok, choose another item that's not {remove_choice}.")
+    else:
+        print(f"Sorry, we don't carry that.")
+    # Tells customer what they want isn't a menu option.
+    
+    get_rating() 
+    # calls the function for user feedback
+    another = input(f"Would you like something else?: ").lower()
+    import sys
+    if another == "yes":
+        print(f"OK, is that what you would like?")
+    else:
+        print(f"OK, bye bye!")
+        sys.exit()
 
-else:
-    print(f"Sorry, we don't carry that.")
-# Tells customer what they want isn't a menu option.
-
-def tell_customer(setup, punchline):
-    input("Hello, welcome to coffe shop! What can I get you?")
-    input(f"{setup}...")
+    def tell_customer(setup, punchline):
+        input("Hello, welcome to coffe shop! Would you like to order anything?")
+        input(f"{setup}...")
     # Pauses for customer response
-    print(f"{punchline}\n")
-    # Delivers punchline
-
+        print(f"{punchline}\n")
+    # Delivers punchline to customer
 
 
 
